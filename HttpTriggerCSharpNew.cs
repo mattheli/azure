5d0f162e2674a57e1f7d2202637ec9166e6a77fa
Matthews <k1801545@kcl.ac.uk>
2018-11-13 16:48:23 +0000

    Working

--- HttpTriggerCSharpNew.cs
+++ HttpTriggerCSharpNew.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace Mattheli.Function
 {
-    public static class MattheliCSharpHTTPTrigger
+    public static class HttpTriggerCSharpNew
     {
-        [FunctionName("MattheliCSharpHTTPTrigger")]
+        [FunctionName("HttpTriggerCSharpNew")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
